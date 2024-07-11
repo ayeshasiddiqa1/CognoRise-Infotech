e259ec38612eb2e23b2b337b68819db4bac8c09f
ayeshasiddiqa1 <ayeshaasiddiqa20@gmail.com>
2024-07-11 17:07:58 +0530

    Add files via upload

---
+++ tictactoeai/constants.py
@@ -0,0 +1,56 @@
+
+
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (0, 0, 0)           # Black background
+LINE_COLOR = (128, 128, 128)   # Gray lines
+CIRC_COLOR = (255, 255, 255)   # White circles
+CROSS_COLOR = (0, 0, 255)      # Blue crosses
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (0, 0, 0)           # Black background
+LINE_COLOR = (128, 128, 128)   # Gray lines
+CIRC_COLOR = (255, 255, 255)   # White circles
+CROSS_COLOR = (0, 0, 255)      # Blue crosses
